4c2be2b89aa6a00065ad75ee4c18bed77251b1c4
Dazum Developer <dazum.dev@gmail.com>
2025-10-13 19:58:23 +0000

    feat(dev): basic dev server

--- packages/dev/CHANGELOG.md
+++ packages/dev/CHANGELOG.md
@@ -1,1 +1,7 @@
 # @toolbox/dev CHANGELOG
+
+## v0.1.0 [12-10-2025]
+
+### Added
+
+- basic dev server
---
+++ packages/dev/eslint.config.mjs
@@ -0,0 +1,4 @@
+import { config } from '@toolbox/eslint-config/react-internal'
+
+/** @type {import("eslint").Linter.Config} */
+export default config
