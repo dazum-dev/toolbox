ca342c7b89a00706b33c6ce374e0b27e3c194756
Dazum Developer <dazum.dev@gmail.com>
2025-10-21 20:01:23 +0530

    feat(@toolbox/plguin-react): migrate from turbo to nx

--- packages/eslint-config/base.js
+++ packages/eslint-config/base.js
@@ -56,6 +56,10 @@ export default [
               sourceTag: 'scope:config',
               onlyDependOnLibsWithTags: ['type:config'],
             },
+            {
+              sourceTag: 'scope:plugin',
+              onlyDependOnLibsWithTags: ['scope:config', 'type:config'],
+            },
           ],
         },
       ],
---
+++ packages/plugin-react/eslint.config.mjs
@@ -0,0 +1,22 @@
+import baseConfig from '@toolbox/eslint-config/base.js'
+
+export default [
+  ...baseConfig,
+  {
+    files: ['**/*.json'],
+    rules: {
+      '@nx/dependency-checks': [
+        'error',
+        {
+          ignoredFiles: [
+            '{projectRoot}/eslint.config.{js,cjs,mjs,ts,cts,mts}',
+            '{projectRoot}/rollup.config.{js,ts,mjs,mts,cjs,cts}',
+          ],
+        },
+      ],
+    },
+    languageOptions: {
+      parser: await import('jsonc-eslint-parser'),
+    },
+  },
+]
