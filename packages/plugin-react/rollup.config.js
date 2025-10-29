699019d63b722f47f7d044b52c7a351fe96c7051
Dazum Developer <dazum.dev@gmail.com>
2025-10-29 21:00:03 +0530

    build(@toolbox/plugin-react): cache build and lint operations and update build config

--- packages/plugin-react/rollup.config.js
+++ packages/plugin-react/rollup.config.js
@@ -3,7 +3,7 @@ import { withNx } from '@nx/rollup/with-nx.js'
 export default withNx(
   {
     main: './src/index.ts',
-    outputPath: '../../dist/packages/plugin-react',
+    outputPath: './dist',
     tsConfig: './tsconfig.lib.json',
     compiler: 'swc',
     format: ['esm'],
